a1dbe4212aa74e37b39fe6b23bb08a6849bcf201
Petar Jovanovic <petarj@protonmail.ch>
2016-01-17 23:56:47 +0100

    nova pitanja

--- shaderrider/platform/pyocl/elementwise.py
+++ shaderrider/platform/pyocl/elementwise.py
@@ -4,6 +4,7 @@ WRITEME
 
 import pyopencl as cl
 
+from shaderrider.symbolic import vast as ast
 from shaderrider.generator import codegen
 
 
@@ -27,6 +28,14 @@ class ElementwiseEval(codegen.OpEvaluator):
 class ElementwiseGenerator(codegen.OpEvalGenerator):
     def generate(self, op):
         # TODO get atomics (inputs), and figure out the output type and dimension
+        atoms = op.getAtoms()   # TODO what about constants?
+        argstr = ''
+        for a in atoms:
+            if a.isArray():
+                # TODO kojim redosledom ovo ide?
+                pass
+            else:   # scalar
+                pass
         # TODO generate C code for the operation (what is the PYOPENCL_ELWISE_CONTINUE exactly?)
         # TODO create the evaluator class (how?)
         pass
