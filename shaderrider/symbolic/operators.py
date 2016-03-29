509462c6e81f71523487e6c8b90c9d32f79ace52
Petar Jovanovic <petarj@protonmail.ch>
2016-03-29 23:16:04 +0200

    prebacio blas, stao kod scan operacija

--- shaderrider/symbolic/operators.py
+++ shaderrider/symbolic/operators.py
@@ -291,8 +291,129 @@ class ElementwiseOP(exprgraph.Operator):
 
 # SCAN OPS ############################################################
 
+# sum, max, min, mean, std,
+
 
 # BLAS OPS ############################################################
+
+class GemmOP(exprgraph.Operator):
+    _type_name = "Gemm"
+
+    def __init__(self, A, B, C, alpha=exprgraph.Constant(1.0), beta=exprgraph.Constant(0.0),
+                 transA=exprgraph.Constant(False), transB=exprgraph.Constant(False), parent=None):
+        super(GemmOP, self).__init__(7, [A, B, C, alpha, beta, transA, transB], parent)
+
+        if len(A.get_shape()) != 2 or len(B.get_shape()) != 2 or len(C.get_shape()) != 2:
+            raise ValueError
+            # TODO
+
+    def substitute(self, a, b):
+        if self == a:
+            return b
+        return GemmOP(self.operands[0].substitute(a, b),
+                      self.operands[1].substitute(a, b),
+                      self.operands[2].substitute(a, b),
+                      self.operands[3],
+                      self.operands[4],
+                      self.operands[5],
+                      self.operands[6])
+
+    def __str__(self):
+        fstr = 'GEMM(%f * %s'
+        if self.operands[5].value:  # transA
+            fstr += "'"
+        fstr += " * %s"
+        if self.operands[6].value:  # transB
+            fstr += "'"
+        fstr += " + %f * %s)"
+        return fstr % map(str, (self.operands[3].value, self.operands[0].name,
+                                self.operands[1].name, self.operands[4].value, self.operands[2].name))
+
+    def get_shape(self):
+        # TODO
+        pass
+
+    def gradient(self, wrt):
+        raise NotImplementedError
+
+    def simplify(self):
+        return GemmOP(self.operands[0].simplify(),
+                      self.operands[1].simplify(),
+                      self.operands[2].simplify(),
+                      self.operands[3].simplify(),
+                      self.operands[4].simplify(),
+                      self.operands[5].simplify(),
+                      self.operands[6].simplify())
+
+
+class GemvOP(exprgraph.Operator):
+    _type_name = "Gemv"
+
+    def __init__(self, A, X, Y, alpha=exprgraph.Constant(1.0), beta=exprgraph.Constant(0.0),
+                 transA=exprgraph.Constant(False), parent=None):
+        super(GemvOP, self).__init__(6, [A, X, Y, alpha, beta, transA], parent)
+        # TODO check dimensions compatibility
+
+    def substitute(self, a, b):
+        if self == a:
+            return b
+        return GemvOP(self.operands[0].substitute(a, b),
+                      self.operands[1].substitute(a, b),
+                      self.operands[2].substitute(a, b),
+                      self.operands[3],
+                      self.operands[4],
+                      self.operands[5])
+
+    def __str__(self):
+        fstr = 'GEMV(%f * %s'
+        if self.operands[5].value:
+            fstr += "'"
+        fstr += ' * %s + %f * %s)'
+        return fstr % map(str, (self.operands[3], self.operands[0], self.operands[1],
+                                self.operands[4], self.operands[2]))
+
+    def gradient(self, wrt):
+        pass
+
+    def get_shape(self):
+        pass
+
+    def simplify(self):
+        pass
+
+
+class GerOP(exprgraph.Operator):
+    _type_name = "Ger"
+
+    def __init__(self, alpha, X, Y, A, parent=None):
+        super(GerOP, self).__init__(4, [alpha, X, Y, A], parent)
+        # TODO check dimensions
+
+        if len(X.get_shape()) != 1 or len(Y.get_shape()) != 1 or len(A.get_shape()) != 2:
+            raise ValueError
+        m, n = X.get_shape()[0], Y.get_shape()[0]
+        if (m, n) != A.get_shape():
+            raise IncompatibleDimensionsError
+
+    def substitute(self, a, b):
+        if self == a:
+            return b
+        return GerOP(self.operands[0],
+                     self.operands[1].substitute(a, b),
+                     self.operands[2].substitute(a, b),
+                     self.operands[3].substitute(a, b))
+
+    def __str__(self):
+        return "GER(%f * %s * %s' + %s)" % map(str, self.operands)
+
+    def get_shape(self):
+        return self.operands[1].get_shape()[0], self.operands[1].get_shape()[0]
+
+    def gradient(self, wrt):
+        raise NotImplementedError
+
+    def simplify(self):
+        pass
 
 
 # CONVOLUTION OPS #####################################################
