d38aec053d39ae9f046bc3de72974486051a2f5d
Petar Jovanovic <petarj@protonmail.ch>
2016-07-30 02:12:23 +0200

    valuacija za pyopencl

--- shaderrider/platform/pyocl/platformdef.py
+++ shaderrider/platform/pyocl/platformdef.py
@@ -201,6 +201,44 @@ class PyOCLValuation(Valuation):
         self._vars.clear()
 
 
+class PyOCLValuation1(dict):
+    def __init__(self, queue,  *args, **kwargs):
+        self._queue = queue
+        self.update(*args, **kwargs)
+
+    # def __getitem__(self, key):
+    #     val = dict.__getitem__(self, key)
+    #     return val
+
+    def __setitem__(self, key, value):
+        if isinstance(value, np.ndarray):
+            val = clarray.to_device(self._queue, value)
+        elif isinstance(value, clarray.Array):
+            val = value
+        else:
+            raise TypeError('Unsupported value type')
+        dict.__setitem__(self, key, val)
+
+    def set_async(self, key, value):
+        if isinstance(value, np.ndarray):
+            val = clarray.to_device(self._queue, value, async=True)
+            dict.__setitem__(self, key, val)
+
+    def get_async(self, key):
+        return self[key].get(async=True)
+
+    def read_value(self, key):
+        return self[key].get()
+
+    def __repr__(self):
+        dictrepr = dict.__repr__(self)
+        return '%s(%s)' % (type(self).__name__, dictrepr)
+
+    def update(self, *args, **kwargs):
+        for k,v in dict(*args, **kwargs).iteritems():
+            self[k] = v
+
+
 #######################################################################
 
 factories = {
