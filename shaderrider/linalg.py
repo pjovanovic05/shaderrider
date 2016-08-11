a0527391a5663e73f7fcca52a68cc37cc4aeacbe
Petar Jovanovic <petarj@protonmail.ch>
2016-08-11 07:36:59 +0200

    mic

--- shaderrider/linalg.py
+++ shaderrider/linalg.py
@@ -12,37 +12,37 @@ from shaderrider.aux import clblaswrap
 from shaderrider import clplatf
 
 
-def dot(a, b, out=None, wait_for=None):
+def dot(queue, a, b, out=None, wait_for=None):
     if len(a.shape) > 2:
         raise TypeError, 'A is not a matrix'
     if len(b.shape) > 2:
         raise TypeError, 'B is not a matrix'
 
-    M, K = a.shape
-    N = b.shape[1]
+    M, K = a.shape if len(a.shape) == 2 else a.shape[0],1
+    N = b.shape[1] if len(b.shape) == 2 else 1
+    ev = None
 
-    q = clplatf.qs[0]   # FIXME
     if out is None:
-        out = clarray.empty(q, (M, N), a.dtype)
+        out = clarray.empty(queue, (M, N), a.dtype)
 
     if M == 1:
         if N == 1:
             # vector dot product
-            scratch = clarray.empty_like(a, queue=q)
-            ev = clblaswrap.dot(q, b, a, out, scratch)
+            scratch = clarray.empty_like(a, queue=queue)
+            ev = clblaswrap.dot(queue, b, a, out, scratch)
         else:
             # gemv where vector is on the left - will need some transpositions
-            ev = clblaswrap.gemv(q, a, b, out)
+            ev = clblaswrap.gemv(queue, a, b, out)
     elif K == 1:
         # outer product
-        ev = clblaswrap.ger(q, out, a, b)
+        ev = clblaswrap.ger(queue, out, a, b)
     elif M > 1:
         if N == 1:
             # standard gemv
-            ev = clblaswrap.gemv(q, a, b, out)
+            ev = clblaswrap.gemv(queue, a, b, out)
         else:
             # gemm finally!
-            ev = clblaswrap.gemm(q, a, b, out)
+            ev = clblaswrap.gemm(queue, a, b, out)
 
     # TODO batch gemm goes to a different op
 
--- test/test_linalg.py
+++ test/test_linalg.py
@@ -1,15 +1,29 @@
 import unittest
+import numpy as np
+from pyopencl import array as clarray
+
+from shaderrider import clplatf
+from shaderrider import linalg
 
 
 class LinalgTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        pass
+        clplatf.init_cl(1)
 
     @classmethod
     def tearDownClass(cls):
         pass
 
     def test_dot(self):
-        pass
+        q = clplatf.qs[0]
+        X = np.random.uniform(0, 1, (10,)).astype(np.float32)
+        Y = np.random.uniform(0, 1, (10,)).astype(np.float32)
+        expected = np.dot(X, Y)
+        gX = clarray.to_device(q, X)
+        gY = clarray.to_device(q, Y)
 
+        gR, ev = linalg.dot(q, gX, gY)
+        ev.wait()
+        R = gR.get()
+        self.assertTrue(np.allclose(R, expected))
